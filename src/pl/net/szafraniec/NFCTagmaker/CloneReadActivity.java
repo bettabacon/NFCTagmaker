d73b4bed53fbb6a5a89b156c4bbd60f618d5cd13
mateusz-szafraniec <szafraniec.m.m@gmail.com>
2014-04-23 10:27:13 +0200

    copyright info

--- src/pl/net/szafraniec/NFCTagmaker/CloneReadActivity.java
+++ src/pl/net/szafraniec/NFCTagmaker/CloneReadActivity.java
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with NFCKey; if not, write to the Free Software
+ * along with NFCTagMaker; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * Ten plik jest częścią NFCTagMaker.
